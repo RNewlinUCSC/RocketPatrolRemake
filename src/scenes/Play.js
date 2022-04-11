c036a460802c3ccc7a444094bf46deac0b2a1c46
Robert Newlin <rnewlin@ucsc.edu>
2022-04-11 12:41:57 -0700

    Restart

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -72,6 +72,11 @@ class Play extends Phaser.Scene {
     }
 
     update() {
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+        this.scene.restart();
+}
+
         this.starfield.tilePositionX -= 4;
         if (!this.gameOver) {
         this.p1Rocket.update();
