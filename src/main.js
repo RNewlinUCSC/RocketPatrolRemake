01312eed4428c3182e3e99d4420dc9545ab509cd
Robert Newlin <rnewlin@ucsc.edu>
2022-04-11 11:24:55 -0700

    Rocket Patrol Tutorial Progress up to creating the Rocket class

---
+++ src/main.js
@@ -0,0 +1,11 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 600,
+    height: 480,
+    scene: [ Menu, Play ],
+};
+
+let game = new Phaser.Game(config);
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
