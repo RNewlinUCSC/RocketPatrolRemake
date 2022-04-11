0d8ac396d69953904124e8db3f9430969df6d288
Robert Newlin <rnewlin@ucsc.edu>
2022-04-11 14:01:31 -0700

    testing a period in front of the explosion call

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,7 +8,7 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
-        this.load.spritesheet('explosion', '/assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
       }
 
     create() {
